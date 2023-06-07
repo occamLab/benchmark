17f5de8657293683e2b6b3c03de1120ab5158db3
Daniel Sudzilouski <daniel@sudzilouski.com>
2023-06-07 13:43:56 -0400

    implement accelerometer message collection (#8)

--- ios_logger2/ios_logger2/Motion.swift
+++ ios_logger2/ios_logger2/Motion.swift
@@ -12,12 +12,29 @@ import CoreMotion
 class Motion {
     public var motionSensors = CMMotionManager()
     
-    private init() {
+    
+    private func initMotionSensors() {
         // Set the update frequencies for gyro, accelerometer, and motion
         let imuUpdateFreqMillis: Double = 10
         let imuUpdateFreqSeconds: Double = imuUpdateFreqMillis /  1000
         motionSensors.gyroUpdateInterval = imuUpdateFreqSeconds
         motionSensors.accelerometerUpdateInterval = imuUpdateFreqSeconds
         motionSensors.deviceMotionUpdateInterval = imuUpdateFreqSeconds
+        
+        // start collecting data
+        motionSensors.startGyroUpdates()
+        motionSensors.startAccelerometerUpdates()
+        motionSensors.startDeviceMotionUpdates()
+    }
+    
+    private func stopMotionSensors() {
+        // stop collecting data
+        motionSensors.stopGyroUpdates()
+        motionSensors.stopAccelerometerUpdates()
+        motionSensors.stopDeviceMotionUpdates()
+    }
+    
+    private init() {
+        initMotionSensors()
     }
 }
