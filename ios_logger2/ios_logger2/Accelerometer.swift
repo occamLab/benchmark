495377353f899f6caa3023f58daad54053c53d49
Daniel Sudzilouski <daniel@sudzilouski.com>
2023-06-07 13:42:54 -0400

    implement accelerometer message collection

--- ios_logger2/ios_logger2/Accelerometer.swift
+++ ios_logger2/ios_logger2/Accelerometer.swift
@@ -2,16 +2,32 @@
 //  Accelerometer.swift
 //  ios_logger2
 //
-//  Created by Daniel Sudzilouski on 6/6/23.
-//
 
 import Foundation
 
 
+/*
+ *  Implements data collection and encoding into a protobuf message for timeseries of accelerometer data
+ */
 class Accelerometer: Sensor {
+    public var series = AccelerometerSeries()
+    
     func collectData(motion: Motion) {
-        var info = BookInfo()
-        info.author = "test"
-      //  motion.motionSensors.
+        if(motion.motionSensors.accelerometerData != nil) {
+            let reading = motion.motionSensors.accelerometerData
+            let timestamp: Double = reading!.timestamp.magnitude
+            let acceleration_x: Double = reading!.acceleration.x
+            let acceleration_y: Double = reading!.acceleration.y
+            let acceleration_z: Double = reading!.acceleration.z
+            
+            
+            var measurement = AccelerometerTimestamp()
+            measurement.timestamp = timestamp
+            measurement.xAcceleration = acceleration_x
+            measurement.yAcceleration = acceleration_y
+            measurement.zAcceleration = acceleration_z
+            series.measurements.append(measurement)
+            
+        }
     }
 }
