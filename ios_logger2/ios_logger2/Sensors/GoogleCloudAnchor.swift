6017c056bca53fe2f8eeb51fbdb4012230fb94f0
Daniel Sudzilouski <daniel@sudzilouski.com>
2023-06-12 16:49:49 -0400

    add syntax for hosting

--- ios_logger2/ios_logger2/Sensors/GoogleCloudAnchor.swift
+++ ios_logger2/ios_logger2/Sensors/GoogleCloudAnchor.swift
@@ -18,16 +18,34 @@ class GoogleCloudAnchor: Sensor {
      init() {
         var error: NSError?
         let configuration = GARSessionConfiguration()
+        configuration.cloudAnchorMode = .enabled
         garSession = try? GARSession(apiKey: Secrets.garAPIKey, bundleIdentifier: nil)
         garSession?.setConfiguration(configuration, error: &error)
     }
     
     func collectData(motion: CMDeviceMotion?, frame: ARFrame?) {
-
+        guard let frame = frame else {return}
+        let _: GARFrame? = try? garSession?.update(frame)
     }
     
-
-    
+    func additionalUpload() async {
+        let newAnchor = ARAnchor(transform: simd_float4x4())
+        
+        // there does not appear to be an async/await version of hostCloudAnchor so we wrap the callback into a promise using the withCheckedContinuation Swift API
+        let anchorHostResult: (anchorName: String?, anchorState: GARCloudAnchorState)? = await withCheckedContinuation { continuation in
+            do {
+                try garSession?.hostCloudAnchor(newAnchor, ttlDays: 365, completionHandler: { (anchorName: String?, anchorState: GARCloudAnchorState) in
+                    continuation.resume(returning: (anchorName, anchorState))
+                })
+            }
+            catch  {
+                print("[ERROR]: Failed to host cloud anchor", error)
+                continuation.resume(returning: nil)
+            }
+        }
+        guard let anchorHostResult = anchorHostResult else {return}
+        print("[INFO]: Hosted cloud anchor with name]", anchorHostResult.anchorName!)
+    }
     
 }
 
