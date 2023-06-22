223e552fd36b658efc61dc9c825011018b03782c
Daniel Sudzilouski <daniel@sudzilouski.com>
2023-06-22 11:07:04 -0400

    update gitignore

--- anchor/backend/data/firebase.py
+++ anchor/backend/data/firebase.py
@@ -3,15 +3,16 @@ from firebase_admin import credentials
 from firebase_admin import storage
 from firebase_admin import initialize_app
 from uuid import uuid4
+import pathlib
 
+service_account_name: str = "stepnavigation-firebase-adminsdk-service-account.json"
+service_account_path: str = (pathlib.Path(__file__).parent.parent.parent / service_account_name).as_posix()
+# do not prefix bucket name with gs:// https://github.com/firebase/firebase-admin-python/issues/280#issuecomment-484980833
+firebase_bucket_name: str = "stepnavigation.appspot.com"
 
-cred = credentials.Certificate('stepnavigation-firebase-adminsdk-service-account.json')
-initialize_app(cred, {
-    'databaseURL' : 'https:url.firebaseio.com/', 'storageBucket': 'gs://stepnavigation.appspot.com'
-})
-bucket = storage.bucket()
+cred = credentials.Certificate(service_account_path)
+initialize_app(cred)
+
+bucket = storage.bucket(firebase_bucket_name)
 blob = bucket.blob("img-03721_VsLSX1xh.mp4")
-new_token = uuid4()
-metadata  = {"firebaseStorageDownloadTokens": new_token}
-blob.metadata = metadata
 blob.download_to_filename(filename="img-03721_VsLSX1xh.mp4")
