07e54d23a76b451bfd638c0f326d9c1f50e85d70
Daniel Sudzilouski <daniel@sudzilouski.com>
2023-06-22 16:11:26 -0400

    add frame extraction and featurepoint extraction (#48)
    * add frame extraction
    
    * currently self matching for some reason
    
    * add database prefix string too

--- anchor/backend/data/firebase.py
+++ anchor/backend/data/firebase.py
@@ -1,16 +1,19 @@
-from firebase_admin import db
-from firebase_admin import credentials
-from firebase_admin import storage
-from firebase_admin import initialize_app
-import pathlib
+from firebase_admin import credentials, storage, initialize_app
+from pathlib import Path
+import cv2
+import shutil
+import tempfile
 
 
 class FirebaseDownloader:
     service_account_name: str = "stepnavigation-firebase-adminsdk-service-account.json"
-    service_account_path: str = (pathlib.Path(__file__).parent.parent.parent / service_account_name).as_posix()
+    service_account_path: str = (Path(__file__).parent.parent.parent / service_account_name).as_posix()
     # do not prefix bucket name with gs:// https://github.com/firebase/firebase-admin-python/issues/280#issuecomment-484980833
     firebase_bucket_name: str = "stepnavigation.appspot.com"
     initialized: bool = False
+
+    # the root of where we download
+    root_download_dir: Path = Path(tempfile.gettempdir()) / "benchmark"
 
     def __init__(self):
         # initialize_app should be called only once globally
@@ -24,9 +27,40 @@ class FirebaseDownloader:
         blob = bucket.blob(remote_location)
         blob.download_to_filename(filename=local_location)
 
+    def extract_ios_logger_tar(self, firebaseDir: str, tarName: str) -> Path:
+        FirebaseDownloader.root_download_dir.mkdir(parents=True, exist_ok=True)
+        if (FirebaseDownloader.root_download_dir / tarName).exists():
+            print(f'[INFO]: Skipping download tar {tarName} as it already exists')
+        else:
+            self.download_file((Path(firebaseDir) / tarName).as_posix(), (FirebaseDownloader.root_download_dir / tarName).as_posix())
+            print(f'[INFO]: Downloaded tar {tarName} as it has not been found locally')
 
-downloader_1 = FirebaseDownloader()
-downloader_1.download_file("img-03721_VsLSX1xh.mp4", "test1.mp4")
+        # unpack the tar itself and cleanup and previous extractions
+        extract_path: Path = FirebaseDownloader.root_download_dir / Path(tarName).stem
+        shutil.rmtree(extract_path)
+        shutil.unpack_archive(FirebaseDownloader.root_download_dir / tarName, extract_dir=extract_path)
 
-downloader_2 = FirebaseDownloader()
-downloader_2.download_file("img-03721_VsLSX1xh.mp4", "test2.mp4")
+        # extract the videos
+        self.extract_ios_logger_video(extract_path / "mapping-video.mp4")
+        self.extract_ios_logger_video(extract_path / "localization-video.mp4")
+
+        return extract_path / "extracted"
+
+    def extract_ios_logger_video(self, video_path: Path):
+        print(f'[INFO]: Extracting video {video_path} to frames')
+        video = cv2.VideoCapture(video_path.as_posix())
+        ret, frame = video.read()
+        frame_num: int = 0
+        while ret:
+            frame_path: Path = video_path.parent / "extracted" / video_path.stem / f'{frame_num}.jpg'
+            frame_path.parent.mkdir(parents=True, exist_ok=True)
+            cv2.imwrite(frame_path.as_posix(), frame)
+            ret, frame = video.read()
+            frame_num += 1
+        video.release()
+
+# test the extractor here
+if __name__ == '__main__':
+    downloader_1 = FirebaseDownloader()
+    downloader_1.extract_ios_logger_tar("iosLoggerDemo/DQP1QbWk6WVZOFN6OpZiQXsfpsB3",
+                                        "27FB2D9E-5898-4DC9-97AB-7D08F231E649.tar")
