d45cd5e187b82913e93c4e9586318fe9b686a3d0
Daniel Sudzilouski <daniel@sudzilouski.com>
2023-06-22 10:25:38 -0400

    gitignore

---
+++ anchor/README.md
@@ -0,0 +1,15 @@
+# Anchor Backend
+
+# Install 
+
+Init the third_party submodule hloc with: 
+```
+git submodule update --init --recursive
+```
+
+To install the pip dependencies make sure that you are in a clean python-3.10 virtualenv and run:
+```
+pip install -r requirements.txt
+```
+
+# Credentials
---
+++ anchor/backend/data/firebase.py
@@ -0,0 +1,17 @@
+from firebase_admin import db
+from firebase_admin import credentials
+from firebase_admin import storage
+from firebase_admin import initialize_app
+from uuid import uuid4
+
+
+cred = credentials.Certificate('credentials.JSON')
+initialize_app(cred, {
+    'databaseURL' : 'https:url.firebaseio.com/', 'storageBucket': 'gs://stepnavigation.appspot.com'
+})
+bucket = storage.bucket()
+blob = bucket.blob("img-03721_VsLSX1xh.mp4")
+new_token = uuid4()
+metadata  = {"firebaseStorageDownloadTokens": new_token}
+blob.metadata = metadata
+blob.download_to_filename(filename="img-03721_VsLSX1xh.mp4")
