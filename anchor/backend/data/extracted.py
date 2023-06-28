eaa96b588eb2ca0eebdefac182dd98f8cfd778c9
Daniel Sudzilouski <daniel@sudzilouski.com>
2023-06-28 11:25:29 -0400

    add timestamp matching algo for image frames (#62)
    * add proper timestamp extraction
    
    * move data object into external class
    
    * frame matching working
    
    * format

---
+++ anchor/backend/data/extracted.py
@@ -0,0 +1,89 @@
+import json
+
+
+class Extracted:
+
+    def __init__(self):
+        self.sensors_extracted = {
+            "mapping_phase": {
+                "intrinsics": [],
+                "poses": [],
+                "video": [],
+            },
+            "localization_phase": {
+                "intrinsics": [],
+                "poses": [],
+                "video": [],
+            }
+        }
+
+    @staticmethod
+    def get_phase_key(mapping_phase: bool):
+        return "mapping_phase" if mapping_phase else "localization_phase"
+
+    # append a video timestamp to the extracted data
+    def append_video_timestamp(self, timestamp: int, frame_num: int, mapping_phase: bool):
+        phase = Extracted.get_phase_key(mapping_phase)
+        self.sensors_extracted[phase]["video"].append({"timestamp": timestamp, "frame_num": frame_num})
+
+    # append a timestamp with intrinsics data
+    def append_intrinsics_data(self, timestamp: int, fx: float, fy: float, cx: float, cy: float, mapping_phase: bool):
+        phase = Extracted.get_phase_key(mapping_phase)
+        intrinsics = {"timestamp": timestamp, "fx": fx, "fy": fy, "cx": cx, "cy": cy}
+        self.sensors_extracted[phase]["intrinsics"].append(intrinsics)
+
+    # append a timestamp with pose data
+    def append_pose_data(self, pose_object: any, mapping_phase: bool):
+        phase = Extracted.get_phase_key(mapping_phase)
+        self.sensors_extracted[phase]["poses"].append(pose_object)
+
+    def match_given_sensor(self, phase: str, match_against: str):
+        """
+            The timestamps that we can get out of the video frames are not entirely accurate. Firstly: the timestamps
+            are encoded based on an arbitrary integer scale known as PTS (presentation timestamp). This means that there
+            will be floating point errors in the timestamps that we read.
+
+            Secondly, the video encoder on the IOS side may drop frames (and this does frequently happen in practice)
+
+            As a result, we need to perform matching on the timestamps to figure out the correspondence between
+            video timestamps and other sensor data timestamps
+        """
+
+        # frames can be re-arranged, so I'm not if the extraction order of the frames is necessarily
+        # the same as their PTS (presentation) order.
+        self.sensors_extracted[phase]["video"].sort(key=lambda x: x["timestamp"])
+
+        # just to be safe, let's sort the sensor we are matching against as well
+        self.sensors_extracted[phase][match_against].sort(key=lambda x: x["timestamp"])
+
+        frame_idx = 0
+        sensor_idx = 0
+        match_count = 0
+
+        while frame_idx < len(self.sensors_extracted[phase]["video"]) and sensor_idx < len(
+                self.sensors_extracted[phase][match_against]):
+            frame_timestamp = self.sensors_extracted[phase]["video"][frame_idx]["timestamp"]
+            sensor_timestamp = self.sensors_extracted[phase][match_against][sensor_idx]["timestamp"]
+
+            # ArFrames feed at roughly 30fps, so we can filter with a 1ms cutoff window to match frames
+            if abs(frame_timestamp - sensor_timestamp) < 0.001:
+                self.sensors_extracted[phase]["video"][frame_idx][match_against] \
+                    = self.sensors_extracted[phase][match_against][sensor_idx]
+                sensor_idx += 1
+                frame_idx += 1
+                match_count += 1
+            elif frame_timestamp > sensor_timestamp:
+                sensor_idx += 1
+            else:
+                frame_idx += 1
+
+        print(
+            f'[INFO]: Matched {match_count} video frames out of {len(self.sensors_extracted[phase]["video"])} total frames in {phase} with {match_against}')
+        assert match_count == len(
+            self.sensors_extracted[phase]["video"]), "failed to match all video frames to sensor data"
+
+    def match_all_sensor(self):
+        for phase in self.sensors_extracted:
+            for sensor in self.sensors_extracted[phase]:
+                if sensor != "video":
+                    self.match_given_sensor(phase, sensor)
--- anchor/backend/data/firebase.py
+++ anchor/backend/data/firebase.py
@@ -1,10 +1,14 @@
 from firebase_admin import credentials, storage, initialize_app
-import proto.intrinsics_pb2 as Intrinsics
-import proto.pose_pb2 as Pose
+
+import anchor.backend.data.extracted
+import anchor.backend.data.proto.intrinsics_pb2 as Intrinsics
+import anchor.backend.data.proto.video_pb2 as video_pb2
+import anchor.backend.data.proto.pose_pb2 as Pose
 from pathlib import Path
 import cv2
 import shutil
 import tempfile
+import av
 
 
 class FirebaseDownloader:
@@ -24,6 +28,16 @@ class FirebaseDownloader:
             initialize_app(cred)
             FirebaseDownloader.initialized = True
 
+        self.extracted_data = anchor.backend.data.extracted.Extracted()
+
+    @staticmethod
+    def proto_with_phase(given_proto: any, mapping_phase: bool):
+        """
+            Gets the phase message from the proto object. We encode all of our protos
+            with the phase that the data was collected in.
+        """
+        return getattr(given_proto, "mappingPhase" if mapping_phase else "localizationPhase")
+
     def download_file(self, remote_location: str, local_location: str):
         bucket = storage.bucket(FirebaseDownloader.firebase_bucket_name)
         blob = bucket.blob(remote_location)
@@ -34,7 +48,8 @@ class FirebaseDownloader:
         if (FirebaseDownloader.root_download_dir / tarName).exists():
             print(f'[INFO]: Skipping download tar {tarName} as it already exists')
         else:
-            self.download_file((Path(firebaseDir) / tarName).as_posix(), (FirebaseDownloader.root_download_dir / tarName).as_posix())
+            self.download_file((Path(firebaseDir) / tarName).as_posix(),
+                               (FirebaseDownloader.root_download_dir / tarName).as_posix())
             print(f'[INFO]: Downloaded tar {tarName} as it has not been found locally')
 
         # unpack the tar itself and cleanup and previous extractions
@@ -44,28 +59,41 @@ class FirebaseDownloader:
 
         # extract the videos
 
-        self.extract_ios_logger_video(extract_path / "mapping-video.mp4")
-        self.extract_ios_logger_video(extract_path / "localization-video.mp4")
-        self.extract_intrinsics(extract_path)
-        self.extract_pose(extract_path)
+        self.extract_ios_logger_video(extract_path / "mapping-video.mp4", True)
+        self.extract_ios_logger_video(extract_path / "localization-video.mp4", False)
+
+        self.extract_intrinsics(extract_path, True)
+        self.extract_intrinsics(extract_path, False)
+
+        self.extract_pose(extract_path, True)
+        self.extract_pose(extract_path, False)
+
+        # print(self.extracted_data.sensors_extracted)
+        self.extracted_data.match_all_sensor()
+
 
         return extract_path / "extracted"
 
-    def extract_ios_logger_video(self, video_path: Path):
+    def extract_ios_logger_video(self, video_path: Path, mapping_phase: bool):
         print(f'[INFO]: Extracting video {video_path} to frames')
-        video = cv2.VideoCapture(video_path.as_posix())
-        ret, frame = video.read()
-        frame_num: int = 0
-        while ret:
-            frame_path: Path = video_path.parent / "extracted" / video_path.stem / f'{frame_num}.jpg'
-            frame_path.parent.mkdir(parents=True, exist_ok=True)
-            cv2.imwrite(frame_path.as_posix(), frame)
-            ret, frame = video.read()
-            frame_num += 1
-        video.release()
-    
+        metadata_path = video_path.parent / "video.proto"
+        video_metadata = video_pb2.VideoData()
 
-    def extract_intrinsics(self, extract_path: Path):
+        with open(metadata_path, "rb") as fd:
+            video_metadata.ParseFromString(fd.read())
+
+        video_start = FirebaseDownloader.proto_with_phase(video_metadata, mapping_phase).videoAttributes.videoStartUnixTimestamp
+
+        container = av.open(video_path.as_posix())
+
+        for frame in container.decode():
+            image_timestamp = video_start + float(frame.pts * frame.time_base)
+            frame_path: Path = video_path.parent / "extracted" / video_path.stem / f'{frame.index}.jpg'
+            frame_path.parent.mkdir(parents=True, exist_ok=True)
+            frame.to_image().save(frame_path.as_posix())
+            self.extracted_data.append_video_timestamp(image_timestamp, frame.index, mapping_phase)
+
+    def extract_intrinsics(self, extract_path: Path, mapping_phase: bool):
         """
         Returns a list of dictionaries each containing a timestamp and the four
         intrinsics for each frame in the video's mapping phase.
@@ -73,28 +101,24 @@ class FirebaseDownloader:
         Args: 
             extract_path (str): the path to the folder containing the video 
             we are extracting and getting the intrinsics data from.
+            mapping_phase (bool): determines which phase we are in (mapping/localization)
         
-        Returns:
-            A list of dictionaries; each dictionary has attributes timestamp,
-            fx, fy, cx, and cy for a particular frame.
+        Returns: (void)
+            Appends data to the Extracted class
         """
-        
+
         print(f'[INFO]: Reading intrinsics protobuf {extract_path}')
         intrinsics_path = extract_path / "intrinsics.proto"
         intrinsics_data = Intrinsics.IntrinsicsData()
         with open(intrinsics_path, "rb") as fd:
             intrinsics_data.ParseFromString(fd.read())
-            intrinsics_list = []
-            for value in intrinsics_data.mappingPhase.measurements:
+            for value in FirebaseDownloader.proto_with_phase(intrinsics_data, mapping_phase).measurements:
                 t = value.timestamp
                 k = value.cameraIntrinsics
                 fx, fy, cx, cy = k[0], k[4], k[6], k[7]
-                intrinsics = {"timestamp": t, "fx": fx, "fy": fy, "cx": cx, "cy": cy}
-                intrinsics_list.append(intrinsics)
-            
-            return(intrinsics_list)
-        
-    def extract_pose(self, extract_path: Path):
+                self.extracted_data.append_intrinsics_data(t, fx, fy, cx, cy, mapping_phase)
+
+    def extract_pose(self, extract_path: Path, mapping_phase: bool):
         """
         Returns a list of dictionaries each containing a timestamp, 
         translation, rotation matrix, and the real and imaginary parts of the 
@@ -103,8 +127,12 @@ class FirebaseDownloader:
         Args: 
             extract_path (str): the path to the folder containing the video 
             we are extracting and getting the pose data from.
+            mapping_phase (bool): determines which phase we are in (mapping/localization)
+
         
-        Returns:
+        Returns: (void)
+            Appends data to the Extracted class with the following form:
+
             A list of dictionaries; each dictionary has the form: 
             {
                 timestamp (float): timestamp from ARFrame
@@ -117,26 +145,25 @@ class FirebaseDownloader:
                 quatReal (float): real part of the quaternion
             }
         """
-        
+
         print(f'[INFO]: Reading pose protobuf {extract_path}')
         pose_path = extract_path / "pose.proto"
         pose_data = Pose.PoseData()
         with open(pose_path, "rb") as fd:
             pose_data.ParseFromString(fd.read())
-            pose_list = []
-            for value in pose_data.mappingPhase.measurements:
+            for value in FirebaseDownloader.proto_with_phase(pose_data, mapping_phase).measurements:
                 t = value.timestamp
                 translation = value.poseTranslation
                 rotationMatrix = value.rotMatrix
                 quatImag = value.quatImag
                 quatReal = value.quatReal
-                pose = {"timestamp": t, "translation": translation, "rotationMatrix": rotationMatrix, "quatImag": quatImag, "quatReal": quatReal}
-                pose_list.append(pose)
-            
-            return pose_list
-        
+                pose = {"timestamp": t, "translation": translation, "rotationMatrix": rotationMatrix,
+                        "quatImag": quatImag, "quatReal": quatReal}
+                self.extracted_data.append_pose_data(pose, mapping_phase)
+
+
 # test the extractor here
 if __name__ == '__main__':
     downloader_1 = FirebaseDownloader()
     downloader_1.extract_ios_logger_tar("iosLoggerDemo/Ljur5BYFXdhsGnAlEsmjqyNG5fJ2",
-                                        "EE27F9BE-63AC-4343-B149-1816EC6EF2C7.tar")
+                                        "047F9850-20BB-4AC0-9650-C2558C9EFC03.tar")
