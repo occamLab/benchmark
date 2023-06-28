82eb89e1c5c14a1d8f3faf73b3aa107b33f375dd
Zoë McGinnis <zoekauai@gmail.com>
2023-06-28 15:07:00 -0400

    Remove duplicated matrix entry (#66)

--- ios_logger2/ios_logger2/Sensors/Pose.swift
+++ ios_logger2/ios_logger2/Sensors/Pose.swift
@@ -47,7 +47,7 @@ extension simd_float4x4 {
         return [self[3,0], self[3,1], self[3,2]]
     }
     public func rotationMatrix()->[Float] {
-        return [self[0,0], self[0,1], self[0,2], self[0,3], self[1,0], self[1,1], self[1,2], self[1,3], self[1,3], self[2,0], self[2,1], self[2,2], self[2,3], self[3,0], self[3,1], self[3,2], self[3,3]]
+        return [self[0,0], self[0,1], self[0,2], self[0,3], self[1,0], self[1,1], self[1,2], self[1,3], self[2,0], self[2,1], self[2,2], self[2,3], self[3,0], self[3,1], self[3,2], self[3,3]]
     }
 }
 
