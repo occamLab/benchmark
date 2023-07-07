df1cd1b41c3a9c4b43f96bf774bd3c318a9b47ab
Daniel Sudzilouski <daniel@sudzilouski.com>
2023-07-07 11:30:10 -0400

    rebase

--- ios_logger2/ios_logger2/Sensors/GoogleCloudAnchor.swift
+++ ios_logger2/ios_logger2/Sensors/GoogleCloudAnchor.swift
@@ -9,11 +9,15 @@ import Foundation
 import CoreMotion
 import ARCore
 import ARCoreGARSession
+import ARCoreCloudAnchors
 
 class GoogleCloudAnchor: Sensor, SensorProtocol {
+    var isResolvingAnchors = false
+    var resolvedAnchors = false
     var sensorName: String = "google_cloud_anchor"
     var series: GoogleCloudAnchorData = GoogleCloudAnchorData()
     var garSession: GARSession?
+    var resolvedAnchorIdentifier: UUID?
 
      override init() {
          var error: NSError?
@@ -22,10 +26,53 @@ class GoogleCloudAnchor: Sensor, SensorProtocol {
          garSession = try? GARSession(apiKey: Secrets.garAPIKey, bundleIdentifier: nil)
          garSession?.setConfiguration(configuration, error: &error)
     }
-    
+
     func collectData(motion: CMDeviceMotion?, frame: ARFrame?, arView: ARSCNView) {
-        guard let frame = frame else {return}
-        let _: GARFrame? = try? garSession?.update(frame)
+        guard let frame = frame else {
+            return
+        }
+        guard let garFrame = try? garSession?.update(frame) else {
+            return
+        }
+        if self.resolvedAnchors == false {
+            if isLocalizationPhase() {
+                if self.isResolvingAnchors == false {
+                    self.isResolvingAnchors = true
+                    print("Localization phase")
+                    do {
+                        try garSession?.resolveCloudAnchor(series.mappingPhase.cloudAnchorMetadata.cloudAnchorName) { garAnchor, cloudState in
+                            guard let garAnchor = garAnchor else {
+                                print("Unable to resolve anchor")
+                                return
+                            }
+                            print("Resolved anchor \(garAnchor.identifier)")
+                            self.resolvedAnchorIdentifier = garAnchor.identifier
+                            self.resolvedAnchors = true
+                        }
+                    } catch {
+                        // print("error \(error.localizedDescription)")
+                    }
+                }
+            }
+        } else {
+            if let cloudAnchor = garFrame.anchors.first(where: { $0.identifier == resolvedAnchorIdentifier }) {
+                let timestamp: Double = getUnixTimestamp(moment: frame.timestamp)
+                let anchor_translation: [Float] = cloudAnchor.transform.translationValuesCA()
+                let anchor_rot_matrix: [Float] = cloudAnchor.transform.rotationMatrixCA()
+                let arkit_transform = frame.camera.transform
+                let arkit_translation: [Float] = arkit_transform.translationValues()
+                let arkit_rot_matrix: [Float] = arkit_transform.rotationMatrix()
+                var cloudAnchorMetadata = CloudAnchorMetadata()
+                cloudAnchorMetadata.timestamp = timestamp
+                cloudAnchorMetadata.cloudAnchorName = series.mappingPhase.cloudAnchorMetadata.cloudAnchorName
+                cloudAnchorMetadata.resolvedCloudAnchorName = resolvedAnchorIdentifier!.uuidString
+                cloudAnchorMetadata.anchorTranslation = anchor_translation
+                cloudAnchorMetadata.anchorRotMatrix = anchor_rot_matrix
+                cloudAnchorMetadata.arkitTranslation = arkit_translation
+                cloudAnchorMetadata.arkitRotMatrix = arkit_rot_matrix
+                series.localizationPhase.cloudAnchorMetadata.append(cloudAnchorMetadata)
+            }
+        }
     }
     
     func additionalUpload() async {
@@ -61,4 +108,24 @@ class GoogleCloudAnchor: Sensor, SensorProtocol {
         }
     }
     
+}
+extension matrix_float4x4 {
+    // Original matrix has form:
+    //    [ a x x 0 ]
+    //    [ x x x 0 ]
+    //    [ x x x 0 ]
+    //    [ x y z 1 ]
+    // Change from matrix_float4x4 to arrays of floats
+    
+    public func translationValuesCA()->[Float] {
+        return [self[3,0], self[3,1], self[3,2]]
+    }
+    public func rotationMatrixCA()->[Float] {
+        return [
+            self[0,0], self[0,1], self[0,2], self[0,3],
+            self[1,0], self[1,1], self[1,2], self[1,3],
+            self[2,0], self[2,1], self[2,2], self[2,3],
+            self[3,0], self[3,1], self[3,2], self[3,3]
+        ]
+    }
 }
