8804acf1c2e2114357131aff2a819f2314cbfdf2
Zoë McGinnis <zoekauai@gmail.com>
2023-07-07 13:32:28 -0400

    Resolve cloud anchors (#84)
    * rebase
    
    * rebase
    
    * more rebase bugs
    
    * enable anchor
    
    * Clean up cloud anchor print statements
    
    * Clean up April tag print statements
    
    ---------
    
    Co-authored-by: Daniel Sudzilouski <daniel@sudzilouski.com>

--- ios_logger2/ios_logger2/Sensors/GoogleCloudAnchor.swift
+++ ios_logger2/ios_logger2/Sensors/GoogleCloudAnchor.swift
@@ -9,11 +9,14 @@ import Foundation
 import CoreMotion
 import ARCore
 import ARCoreGARSession
+import ARCoreCloudAnchors
 
 class GoogleCloudAnchor: Sensor, SensorProtocol {
+    var isResolvingAnchors = false
     var sensorName: String = "google_cloud_anchor"
     var series: GoogleCloudAnchorData = GoogleCloudAnchorData()
     var garSession: GARSession?
+    var resolvedAnchorIdentifier: UUID?
 
      override init() {
          var error: NSError?
@@ -24,8 +27,56 @@ class GoogleCloudAnchor: Sensor, SensorProtocol {
     }
     
     func collectData(motion: CMDeviceMotion?, frame: ARFrame?, arView: ARSCNView) {
-        guard let frame = frame else {return}
-        let _: GARFrame? = try? garSession?.update(frame)
+        guard let frame = frame else {
+            return
+        }
+        guard let garFrame = try? garSession?.update(frame) else {
+            return
+        }
+        // Make sure the rest happens only if we're in the localization phase
+        guard isLocalizationPhase() else {
+            return
+        }
+        // Make sure we are only trying to resolve one anchor at a time
+        guard self.isResolvingAnchors else {
+            self.isResolvingAnchors = true
+            do {
+                try garSession?.resolveCloudAnchor(series.mappingPhase.cloudAnchorMetadata.cloudAnchorName) { garAnchor, cloudState in
+                    guard let garAnchor = garAnchor else {
+                        print("[ERROR]: Unable to resolve anchor")
+                        return
+                    }
+                    print("[INFO]: Resolved anchor \(garAnchor.identifier)")
+                    self.resolvedAnchorIdentifier = garAnchor.identifier
+                    self.isResolvingAnchors = false
+                }
+            } catch {
+                // print("error \(error.localizedDescription)")
+            }
+            sendLocalizationData(frame: frame, cloudAnchors: garFrame)
+            return
+        }
+    }
+    
+    func sendLocalizationData(frame: ARFrame, cloudAnchors: GARFrame) {
+        guard let cloudAnchor = cloudAnchors.updatedAnchors.first(where: { $0.identifier == resolvedAnchorIdentifier }) else {
+            return
+        }
+        let timestamp: Double = getUnixTimestamp(moment: frame.timestamp)
+        let anchor_translation: [Float] = cloudAnchor.transform.translationValues()
+        let anchor_rot_matrix: [Float] = cloudAnchor.transform.rotationMatrix()
+        let arkit_transform = frame.camera.transform
+        let arkit_translation: [Float] = arkit_transform.translationValues()
+        let arkit_rot_matrix: [Float] = arkit_transform.rotationMatrix()
+        var cloudAnchorMetadata = CloudAnchorMetadata()
+        cloudAnchorMetadata.timestamp = timestamp
+        cloudAnchorMetadata.cloudAnchorName = series.mappingPhase.cloudAnchorMetadata.cloudAnchorName
+        cloudAnchorMetadata.resolvedCloudAnchorName = resolvedAnchorIdentifier!.uuidString
+        cloudAnchorMetadata.anchorTranslation = anchor_translation
+        cloudAnchorMetadata.anchorRotMatrix = anchor_rot_matrix
+        cloudAnchorMetadata.arkitTranslation = arkit_translation
+        cloudAnchorMetadata.arkitRotMatrix = arkit_rot_matrix
+        series.localizationPhase.cloudAnchorMetadata.append(cloudAnchorMetadata)
     }
     
     func additionalUpload() async {
@@ -56,9 +107,8 @@ class GoogleCloudAnchor: Sensor, SensorProtocol {
                 print("[ERROR]: Failed to host cloud anchor: ", anchorHostResult.anchorState)
                 return
             }
-            print("[INFO]: Hosted cloud anchor with name]", anchorName)
+            print("[INFO]: Hosted cloud anchor with name", anchorName)
             series.mappingPhase.cloudAnchorMetadata.cloudAnchorName = anchorName
         }
     }
-    
 }
