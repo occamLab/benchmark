4b7e80c1a96e7fde00ec80dc4f28218c68377065
Daniel Sudzilouski <daniel@sudzilouski.com>
2023-07-12 16:08:25 -0400

    move submodule (#93)
    Co-authored-by: Daniel Sudzilouski <>

--- anchor/backend/scripts/install.sh
+++ anchor/backend/scripts/install.sh
@@ -2,15 +2,19 @@
 
 # run this script with source ./install.sh so that the environment can be propagated to the parent shell
 
+# change directory to root of repo regardless of where file has been executed from
+# https://stackoverflow.com/questions/3349105/how-can-i-set-the-current-working-directory-to-the-directory-of-the-script-in-ba
+cd "$(dirname "${BASH_SOURCE[0]}")"
+cd ../../../
 
 # initialize the base environment from https://github.com/nianticlabs/ace/blob/main/environment.yml
 source ~/anaconda3/etc/profile.d/conda.sh
 conda create --name anchor python --no-default-packages
-conda env update -f ../third_party/ace/environment.yml --name anchor --prune
+conda env update -f anchor/third_party/ace/environment.yml --name anchor --prune
 conda activate anchor
 
 # install ace subdependency
-pip install -e ../third_party/ace/dsacstar
+pip install -e anchor/third_party/ace/dsacstar
 # create debug videos of training process in ace
 sudo apt install ffmpeg -y
 
