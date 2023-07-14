5cea7dfcaf85501267f4aad115e78e52a5cad3b0
Daniel Sudzilouski <daniel@sudzilouski.com>
2023-07-14 11:11:07 -0400

    bridging headers now compile

---
+++ ios_logger2/ios_logger2/Model/TorchModule.h
@@ -0,0 +1,21 @@
+// Copyright (c) 2020 Facebook, Inc. and its affiliates.
+// All rights reserved.
+//
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree.
+
+
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TorchModule : NSObject
+
++ (instancetype)new NS_UNAVAILABLE;
+- (instancetype)init NS_UNAVAILABLE;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios_logger2/ios_logger2/Model/TorchModule.mm
@@ -0,0 +1,14 @@
+// Copyright (c) 2020 Facebook, Inc. and its affiliates.
+// All rights reserved.
+//
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree.
+
+#import "TorchModule.h"
+#include <LibTorch-Lite/LibTorch-Lite.h>
+
+@implementation TorchModule {
+@protected
+    torch::jit::script::Module _impl;
+}
+@end
