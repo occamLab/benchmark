2bc54761a89311b6f80c0580f123920d526cfede
Daniel Sudzilouski <daniel@sudzilouski.com>
2023-07-14 14:35:16 -0400

    start predictPose file

---
+++ ios_logger2/ios_logger2/Model/PredictPose.swift
@@ -0,0 +1,19 @@
+//
+//  PredictPose.swift
+//  ios_logger2
+//
+//  Created by Daniel Sudzilouski on 7/14/23.
+//
+
+import Foundation
+
+
+func loadModule() {
+    if let filePath = Bundle.main.path(forResource: "model", ofType: "pt"),
+        let module = TorchModule(fileAtPath: filePath) {
+        return module
+    } else {
+        fatalError("Can't find the model file!")
+    }
+}
+
