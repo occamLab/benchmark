6ab164c908d269472ce5b9b83b12905eaa4eacc6
mickmcginnis <zoekauai@gmail.com>
2023-07-17 10:08:45 -0400

    Create server training pipeline

---
+++ anchor/backend/scripts/while.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "---------------------NOTICE--------------------------------"
+echo "while.sh assumes that install.sh has already been run"
+echo "---------------------NOTICE--------------------------------"
+
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate anchor
+
+# change directory to root of repo regardless of where file has been executed from
+# https://stackoverflow.com/questions/3349105/how-can-i-set-the-current-working-directory-to-the-directory-of-the-script-in-ba
+cd "$(dirname "${BASH_SOURCE[0]}")"
+cd ../../../
+
+export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
+
+# runs continuously, checking for new uploads to iosLoggerDemo/tarQueue/
+# trains if new tar file is found, deletes the tar file and uploads the model to iosLoggerDemo/trainedModels/
+while true
+do
+    python -m anchor.backend.data.ace
+done
