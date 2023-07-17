81797553932cf18a04f1fdb28500e5c57ce7e8f9
Daniel Sudzilouski <daniel@sudzilouski.com>
2023-07-17 15:48:24 -0400

    start localization server (#100)
    * add boilerplate
    
    * connectable
    
    * downloads the file
    
    * add loader cache
    
    ---------
    
    Co-authored-by: Daniel Sudzilouski <>

---
+++ anchor/backend/server/localizer.py
@@ -0,0 +1,34 @@
+from typing import Union
+from fastapi import FastAPI
+from pydantic import BaseModel
+from pathlib import Path
+from anchor.third_party.ace.ace_network import Regressor
+from anchor.backend.server.loaders import ModelLoader
+import torch
+
+from anchor.backend.data.firebase import FirebaseDownloader
+
+
+# you can start the server by running: 
+#   uvicorn anchor.backend.server.localizer:app --reload --host 10.26.26.130 --port 8000
+
+
+app = FastAPI()
+modelLoader = ModelLoader()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+
+class LocalizeImageReq(BaseModel):
+    base64Jpg: str
+    modelName: str
+
+@app.post("/localize/")
+def localizeImage(req: LocalizeImageReq):
+    model = modelLoader.load_ace_model_if_needed(req.modelName, modelLoader.download_model_if_needed(req.modelName))
+    print(model)
+
+    return {"status": "ok"}
