b723414890b332c68abccebd46b7e03e766bf931
Daniel Sudzilouski <>
2023-07-17 15:04:52 -0400

    downloads the file

--- anchor/backend/server/localizer.py
+++ anchor/backend/server/localizer.py
@@ -1,12 +1,19 @@
 from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
+from pathlib import Path
+import tempfile
+
+from anchor.backend.data.firebase import FirebaseDownloader
 
 
 # you can start the server by running: 
 #   uvicorn anchor.backend.server.localizer:app --reload --host 10.26.26.130 --port 8000
 
+
 app = FastAPI()
+downloader = FirebaseDownloader("none", "none")
+models = {}
 
 @app.get("/")
 def read_root():
@@ -18,4 +25,13 @@ class LocalizeImageReq(BaseModel):
 
 @app.post("/localize/")
 def localizeImage(req: LocalizeImageReq):
+    if(req.modelName in models.keys()): 
+        print("[INFO]: Model already loaded, running inference")
+    else: 
+        tmpFile = tempfile.NamedTemporaryFile().name
+        downloader.download_file((Path("iosLoggerDemo") / "trainedModels" / req.modelName).as_posix(), tmpFile)
+        print(f"[INFO]: Downloaded model {req.modelName}")
+        
+    
+
     return {"status": "ok"}
