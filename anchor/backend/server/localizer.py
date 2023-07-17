680a89d7cc5930843f9ec89cc705d07fb1584794
Daniel Sudzilouski <>
2023-07-17 14:40:00 -0400

    connectable

--- anchor/backend/server/localizer.py
+++ anchor/backend/server/localizer.py
@@ -1,17 +1,21 @@
 from typing import Union
 from fastapi import FastAPI
+from pydantic import BaseModel
 
-# you can start the server by running uvicorn anchor.backend.server.localizer:app --reload
 
+# you can start the server by running: 
+#   uvicorn anchor.backend.server.localizer:app --reload --host 10.26.26.130 --port 8000
 
 app = FastAPI()
-
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+class LocalizeImageReq(BaseModel):
+    base64Jpg: str
+    modelName: str
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/localize/")
+def localizeImage(req: LocalizeImageReq):
+    return {"status": "ok"}
