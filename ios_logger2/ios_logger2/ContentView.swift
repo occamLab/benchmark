994688ecdb87e38a201c03ce8ab09650dc99332e
Zoe McGinnis <zoekauai@gmail.com>
2023-07-18 16:31:48 -0400

    Clean up buttonized code

--- ios_logger2/ios_logger2/ContentView.swift
+++ ios_logger2/ios_logger2/ContentView.swift
@@ -14,76 +14,10 @@ class MotionManager: ObservableObject {
     @Published var mappingComplete = false
     @Published var localizationComplete = false
     
-
-    
     init() {
         print("Init")
-//        // note that UI updates must happen on the main thread which is why DispatchQueue.main.sync is used
-//        Task {
-//            // allow time for alignment of phone
-//            DispatchQueue.main.sync {
-//                self.phaseText = "Align phone to starting position (10 seconds)! HOLD VERTICALLY AGAINST TABLE EDGE (camera staight on). For some reason the Arkit initial pose is absolute garbage if you hold the camera face down."
-//            }
-//             try! await Task.sleep(for: .seconds(10))
-//
-//            DispatchQueue.main.sync {
-//                // initialize
-//                // we don't want to collect data for the first few seconds so that the mapping
-//                // data does not exactly start in the same visual place as the localization data
-//                // that could help the cloud anchor model cheat
-//                self.phaseText = "Walk to a random place to reset the pose."
-//            }
-//            try! await Task.sleep(for: .seconds(10))
-//
-//            // start collecting data
-//            DispatchQueue.main.sync {
-//                self.phaseText = "Currently in mapping phase (20 seconds)."
-//                motion!.disabledCollection = false
-//            }
-//
-//            // allow time for mapping phase
-//            try! await Task.sleep(for: .seconds(20))
-//            DispatchQueue.main.sync {
-//                self.phaseText = "Transitioning between phases!"
-//                motion!.disabledCollection = true
-//            }
-//
-//            // allow time for alignment of phone
-//            await motion!.switchToLocalization()
-//            DispatchQueue.main.sync {
-//                self.phaseText = "Align phone to starting position (10 seconds)! HOLD VERTICALLY AGAINST TABLE EDGE (camera staight on). For some reason the Arkit initial pose is absolute garbage if you hold the camera face down."
-//            }
-//
-//            try! await Task.sleep(for: .seconds(10))
-//            // reset our knowledge of our position
-//            motion!.initMotionSensors()
-//            motion!.initArSession()
-//
-//            // we don't want to collect data for the first few seconds so that the mapping
-//            // data does not exactly start in the same visual place as the localization data
-//            // that could help the cloud anchor model cheat
-//            DispatchQueue.main.sync {
-//                self.phaseText = "Walk to a random place to reset the pose"
-//            }
-//            try! await Task.sleep(for: .seconds(10))
-//
-//
-//            // allow time for localization phase
-//            DispatchQueue.main.sync {
-//                self.phaseText = "Currently in localization phase!!"
-//                motion!.disabledCollection = false
-//            }
-//
-//            try! await Task.sleep(for: .seconds(20)) // allow time for localization phase
-//            DispatchQueue.main.sync {
-//                self.phaseText = "Finished localization phase!!"
-//                self.isPresentingUploadConfirmation = true
-//                motion!.disabledCollection = true
-//            }
-//        }
     }
     func setUpMotion() {
-        print("setUpMotion")
         motion = Motion()
     }
     func mappingPhase() {
@@ -107,7 +41,6 @@ class MotionManager: ObservableObject {
     func localizationPhaseComplete() {
         DispatchQueue.main.asyncAfter(deadline: .now() + 10) {
             self.isPresentingUploadConfirmation = true
-            print("Great")
         }
     }
 }
@@ -205,9 +138,3 @@ struct ContentView: View {
         }
     }
 }
-
-//struct ContentView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ContentView()
-//    }
-//}
