2e359f255ac17a8b429400b00516481dd40ac766
Daniel Sudzilouski <>
2023-07-18 09:24:33 -0400

    rebase

--- anchor/backend/server/localizer.py
+++ anchor/backend/server/localizer.py
@@ -12,6 +12,8 @@ from anchor.backend.data.firebase import FirebaseDownloader
 # you can start the server by running: 
 #   uvicorn anchor.backend.server.localizer:app --reload --host 10.26.26.130 --port 8000
 
+# you can start the server by running: 
+#   uvicorn anchor.backend.server.localizer:app --reload --host 10.26.26.130 --port 8000
 
 app = FastAPI()
 modelLoader = ModelLoader()
@@ -20,6 +22,9 @@ modelLoader = ModelLoader()
 def read_root():
     return {"Hello": "World"}
 
+class LocalizeImageReq(BaseModel):
+    base64Jpg: str
+    modelName: str
 
 
 class LocalizeImageReq(BaseModel):
