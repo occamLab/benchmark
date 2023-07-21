eebc7aa90fd3ea985fa5964cd9755c66ea622f72
Daniel Sudzilouski <daniel@sudzilouski.com>
2023-07-21 14:45:15 -0400

    convert to sklearn image reading (#116)
    * convert to sklearn
    
    * Update anchor/backend/server/loaders.py
    
    ---------
    
    Co-authored-by: Daniel Sudzilouski <>

--- anchor/backend/server/loaders.py
+++ anchor/backend/server/loaders.py
@@ -2,14 +2,14 @@
 from anchor.third_party.ace.ace_network import Regressor
 from anchor.backend.data.firebase import FirebaseDownloader
 from anchor.backend.server.logs import nostdout, nostderr
-import dsacstar
-import torchvision.transforms.functional as TF
 from torchvision import transforms
 from torch.cuda.amp import autocast
 from pathlib import Path
 from io import BytesIO
 from PIL import Image
-import torch, tempfile, base64
+from skimage import io
+import torchvision.transforms.functional as TF
+import torch, tempfile, base64, dsacstar, time, numpy as np
 
 
 class ModelLoader: 
@@ -57,60 +57,56 @@ class ModelLoader:
 
     """
     def localize_image(self, model_name: str, base64Jpg: str, focal_length: float, optical_x: float, optical_y: float):
-        model = self.load_ace_model_if_needed(model_name, self.download_model_if_needed(model_name))
+        with torch.no_grad():
+            model = self.load_ace_model_if_needed(model_name, self.download_model_if_needed(model_name))
 
-        img_bytes: bytes = base64.b64decode(base64Jpg) 
-        with open("example.jpg", "wb") as handle:
-            handle.write(img_bytes)
-        
-        img_file: BytesIO = BytesIO(img_bytes)
-        pil_img: Image = Image.open(img_file)   
-        train_resolution: int = 480
-        original_image_height: int = pil_img.size[1]
+            img_bytes: bytes = base64.b64decode(base64Jpg) 
+            train_resolution: int = 480
 
-        scale_factor = train_resolution / original_image_height
+            pil_image: Image
+            scale_factor: int
+            
+            with tempfile.NamedTemporaryFile() as tmp:
+                tmp.write(img_bytes)
+                sk_image = io.imread(tmp.name)
+                scale_factor = train_resolution / sk_image.shape[0]
+                pil_image = TF.to_pil_image(sk_image)
+                pil_image = TF.resize(pil_image, train_resolution)
 
-        focal_length *= scale_factor
-        optical_x *= scale_factor
-        optical_y *= scale_factor
+            image_transform = transforms.Compose([
+                    transforms.Grayscale(),
+                    transforms.ToTensor(),
+                    transforms.Normalize(
+                        mean=[0.4],  # statistics calculated over 7scenes training set, should generalize fairly well
+                        std=[0.25]
+                    ),
+            ])
+            tensor_image = image_transform(pil_image)
+            tensor_image = tensor_image.half()
+            tensor_image = tensor_image.unsqueeze(0) # this fills the batch_size as 1 by adding a new dimension
+            
+            device = torch.device("cuda")
+            tensor_image = tensor_image.to(device, non_blocking=True)
 
-        image_transform = transforms.Compose([
-                transforms.Resize(train_resolution),
-                transforms.Grayscale(),
-                transforms.ToTensor(),
-                transforms.Normalize(
-                    mean=[0.4],  # statistics calculated over 7scenes training set, should generalize fairly well
-                    std=[0.25]
-                ),
-        ])
-        tensor_image = image_transform(pil_img)
-        tensor_image = tensor_image.unsqueeze(0) # this fills the batch_size as 1 by adding a new dimension
-        
-        device = torch.device("cuda")
-        tensor_image = tensor_image.to(device, non_blocking=True)
+            with autocast(enabled=True):
+                scene_coordinates = model(tensor_image).float().cpu()
+            
+            # Allocate output variable.
+            out_pose = torch.zeros((4, 4))
+            print(scene_coordinates.size())
+            with nostdout():
+                inlier_count: int = dsacstar.forward_rgb(
+                    scene_coordinates,
+                    out_pose,
+                    64, # ransack hypothesis
+                    10, # inlier threshold
+                    focal_length * scale_factor, # focal length
+                    optical_x * scale_factor, # ox
+                    optical_y * scale_factor, # oy
+                    100, # inlier alpha
+                    100, # max pixel error
+                    model.OUTPUT_SUBSAMPLE,
+                )
 
-        with autocast(enabled=True):
-            scene_coordinates = model(tensor_image).float().cpu()
-        
-        # Allocate output variable.
-        out_pose = torch.zeros((4, 4))
-        
-        with nostdout():
-            inlier_count: int = dsacstar.forward_rgb(
-                scene_coordinates,
-                out_pose,
-                64, # ransack hypothesis
-                10, # inlier threshold
-                focal_length, # focal length
-                optical_x, # ox
-                optical_y, # oy
-                100, # inlier alpha
-                100, # max pixel error
-                model.OUTPUT_SUBSAMPLE,
-            )
-
-        if(inlier_count > 200):
-            print(float(out_pose[0,3]), float(out_pose[1,3]), float(out_pose[2,3]))
-
-        return out_pose, inlier_count
-    
+            print(inlier_count)
+            return out_pose, inlier_count
