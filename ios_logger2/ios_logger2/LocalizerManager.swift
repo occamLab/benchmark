92a78499b1f96ffc22df90de868312e226fa97cf
Daniel Sudzilouski <daniel@sudzilouski.com>
2023-07-31 11:54:15 -0400

    typo

--- ios_logger2/ios_logger2/LocalizerManager.swift
+++ ios_logger2/ios_logger2/LocalizerManager.swift
@@ -29,7 +29,6 @@ class LocalizerManager {
             return
         }
         pendingReq = true
-        arkitPose = frame.camera.transform.rotationMatrix()
             
         // req params that we send to the server
         let parameters: [String : Any] = [
@@ -38,7 +37,7 @@ class LocalizerManager {
             "focal_length": frame.camera.intrinsics[0, 0],
             "optical_x": frame.camera.intrinsics[2, 0],
             "optical_y": frame.camera.intrinsics[2, 1],
-            "arkit_pose": arkitPose
+            "arkit_pose": frame.camera.transform.rotationMatrix()
         ]
         
         // set req headers
